612237b0aada2812ae95ed2ae8952f63ab65e354
kaur0222 <kaur0222@algonquinlive.com>
2017-11-09 17:57:58 -0500

    Finished lab 3

--- app/src/main/java/com/example/admin/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/admin/androidlabs/StartActivity.java
@@ -1,13 +1,87 @@
 package com.example.admin.androidlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+private Button startButton;
+    String TAG = "activity_start.xml";
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        startButton = (Button)findViewById(R.id.button);
+        buttonClickAction();
     }
+
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i(TAG, "onDestroy");
+    }
+
+    private void buttonClickAction(){
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                Log.i(TAG, "clicked the button");
+                Intent intent = new Intent(StartActivity.this, ListItemActivity.class);
+                startActivityForResult(intent, 10);
+                onActivityResult(10,10, intent);
+
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int responseCode, Intent data){
+        if(requestCode == 10 ){
+            if(responseCode == Activity.RESULT_OK){
+                Log.i(TAG, "Returned to StartActivity.onActivityResult");
+                String messagePassed = data.getStringExtra("Response");
+                Toast.makeText(this,"my information to share", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+
+
+
 }
